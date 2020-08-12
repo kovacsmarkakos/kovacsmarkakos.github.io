6dcf195ed65ddbd654449eed47eb26b36bd5312c
Mark Kovacs <64911445+kovacsmarkakos@users.noreply.github.com>
2020-08-12 13:26:26 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+let logo = document.querySelector('#logo');
+
+  var tl = anime.timeline({
+    easing: 'easeOutExpo',
+    duration: 700
+  });
+
+  tl
+  .add({
+    targets: '.path',
+    opacity: [0, 1],
+    translateY: ["5rem", "0rem"],
+    scale: [.75, 1],
+    delay: anime.stagger(80, {start: 800}),
+    direction: 'forwards',
+  })
+  .add({
+    targets: '#hammer',
+    rotate: 360,
+    duration: 1300,
+    autoplay: true,
+    easing: 'easeOutBack',
+  }, '-=850');
+
+
+let hammerAnimation = anime({
+  targets: '#hammer',
+    rotate: 360,
+    duration: 800,
+    autoplay: false,
+    easing: 'easeOutBack',
+});
+
+logo.addEventListener('click', function() { hammerAnimation.play(); });
